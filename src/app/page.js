6957a8288a06ee044650064433a5eae3f902462d
Aminul islam <aminurrohman98@gmail.com>
2026-04-28 17:25:17 +0100

    made banner and rendered card

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Banner from "@/components/Banner";
-import Image from "next/image";
-import { HiH1 } from "react-icons/hi2";
+import TopGenerations from "@/components/TopGenerations";
+// import Image from "next/image";
+// import { HiH1 } from "react-icons/hi2";
 
 export default function Home() {
   return (
     <div>
       <Banner></Banner>
+      <TopGenerations></TopGenerations>
     </div>
   );
 }
---
+++ src/components/TopGenerations.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PhotoCard from "./PhotoCard";
+
+const TopGenerations = async () => {
+  const res = await fetch("https://pixgen-gules.vercel.app/data.json");
+  const data = await res.json();
+  const slicedData = data.slice(0, 8);
+  console.log(slicedData);
+  return (
+    <section>
+      <div className="max-w-7xl w-full mx-auto flex flex-col gap-10">
+        <h1 className="font-semibold text-4xl text-center mt-10">
+          Top generations
+        </h1>
+        <div className="grid md:grid-cols-4 grid-cols-1 gap-3">
+          {slicedData.map((photo) => (
+            <PhotoCard key={photo.id} photo={photo}></PhotoCard>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TopGenerations;
