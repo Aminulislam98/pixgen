6f16189686defda337b42aa0731b729c7a630a6c
Aminul islam <aminurrohman98@gmail.com>
2026-04-29 11:56:36 +0100

    updated authclient

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -3,4 +3,3 @@ export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
   baseURL: "https://pixgen-gules.vercel.app/",
 });
-export const { signIn, signUp, useSession } = createAuthClient();
